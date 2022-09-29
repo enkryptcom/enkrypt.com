970f9f92bad893a95be9785aa220aca29ba3717e
Mihail Polev <mpolev364@gmail.com>
2022-09-29 14:47:39 +0700

    update Networks carousel

--- src/views/main/components/main-third-block.vue
+++ src/views/main/components/main-third-block.vue
@@ -10,42 +10,17 @@
           </p>
         </div>
       </div>
+    </div>
+    <div class="main-third__carousels">
+      <networks-carousel duration="100s" direction="normal" />
+      <networks-carousel duration="140s" direction="reverse" />
+      <networks-carousel duration="180s" direction="normal" />
+      <networks-carousel duration="120s" direction="reverse" />
+      <networks-carousel duration="140s" direction="normal" />
+    </div>
+    <div class="container">
       <div class="row justify-content-start">
         <div class="col-11">
-          <div class="main-third__networks">
-            <div class="main-third__networks-item eth">
-              <eth /><span>Ethereum</span>
-            </div>
-            <div class="main-third__networks-item polkadot">
-              <polkadot /><span>Polkadot</span>
-            </div>
-            <div class="main-third__networks-item kusama">
-              <kusama /><span>Kusama</span>
-            </div>
-            <div class="main-third__networks-item polygon">
-              <Polygon /><span>Polygon</span>
-            </div>
-            <div class="main-third__networks-item acala">
-              <acala /><span>Acala</span>
-            </div>
-            <div class="main-third__networks-devider"></div>
-            <div class="main-third__networks-item moonbeam">
-              <moonbeam /><span>Moonbeam</span>
-            </div>
-            <div class="main-third__networks-item bnb">
-              <Bnb /><span>BNB Smart chain</span>
-            </div>
-            <div class="main-third__networks-item karura">
-              <karura /><span>Karura</span>
-            </div>
-            <div class="main-third__networks-item moonriver">
-              <moonriver /><span>Moonriver</span>
-            </div>
-            <div class="main-third__networks-item astar">
-              <img :src="AstarImg" /><span>Astar</span>
-            </div>
-          </div>
-
           <p class="main-third__info">
             Are you a contributor to a specific chain that you would like
             Enkrypt to support?
@@ -62,16 +37,7 @@
 </template>
 
 <script setup lang="ts">
-import Eth from "../../../icons/networks/eth.vue";
-import Polkadot from "../../../icons/networks/polkadot.vue";
-import Kusama from "../../../icons/networks/kusama.vue";
-import Polygon from "../../../icons/networks/polygon.vue";
-import Acala from "../../../icons/networks/acala.vue";
-import Moonbeam from "../../../icons/networks/moonbeam.vue";
-import Bnb from "../../../icons/networks/bnb.vue";
-import Karura from "../../../icons/networks/karura.vue";
-import Moonriver from "../../../icons/networks/moonriver.vue";
-import AstarImg from "../../../icons/networks/astar.png";
+import NetworksCarousel from "../../../components/networks-carousel/index.vue";
 </script>
 
 <style lang="less" scoped>
@@ -143,172 +109,57 @@ import AstarImg from "../../../icons/networks/astar.png";
     });
   }
 
-  &__networks {
+  &__carousels {
+    height: 196px;
+    position: relative;
     margin-bottom: 60px;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex-wrap: wrap;
-    font-size: 0;
+    width: 100%;
 
     .screen-lg({
-      margin-bottom: 65px;
-    });
-
-    .screen-md({
-      margin-bottom: 56px;
-    });
-
-    .screen-sm({
-      margin-bottom: 24px;
+      height: 188px;
     });
 
     .screen-xs({
-      margin-bottom: 65px;
+      height: 322px;
     });
 
-    &-item {
-      padding: 8px 16px 8px 8px;
-      height: 52px;
-      border-radius: 30px;
-      margin-right: 20px;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-      .paragraphBold();
-      letter-spacing: 0.02em;
-      color: @white;
-      text-decoration: none;
+    .screen-lg({
+      margin-bottom: 80px;
+    });
+
+    .screen-sm({
+      margin-bottom: 40px;
+    });
+
+    .vifnslb-container {
       margin-bottom: 20px;
 
       .screen-lg({
-        height: 48px;
         margin-bottom: 16px;
-        margin-right: 16px;
       });
 
-      .screen-md({
-        margin-bottom: 24px;
-      });
+      &:nth-child(3) {
+        margin-bottom: 0;
 
-      .screen-sm({
-        height: 44px;
-        margin-bottom: 16px;
-        margin-right: 16px;
-      });
-
-      &:nth-child(4) {
-        .screen-md({
-          margin-left: 56px;
-        });
-
-        .screen-sm({
-          margin-left: 0;
+        .screen-xs({
+          margin-bottom: 16px;
         });
       }
 
+      &:nth-child(4),
       &:nth-child(5) {
-        margin-right: 0;
+        display: none;
 
-        .screen-md({
-          margin-right: 16px;
-        });
-      }
-
-      &:nth-child(7) {
-        margin-left: 88px;
-
-        .screen-lg({
-          margin-left: 76px;
-        });
-
-        .screen-md({
-          margin-left: 0;
-        });
-      }
-
-      &:nth-child(8) {
-        .screen-md({
-          margin-left: 112px;
-        });
-
-        .screen-sm({
-          margin-left: 0;
+        .screen-xs({
+          display: block;
         });
       }
 
       &:last-child {
-        .screen-md({
-          margin-right: 0;
+        .screen-xs({
+          margin-bottom: 16px;
         });
       }
-
-      svg {
-        width: 36px;
-        height: 36px;
-        margin-right: 8px;
-
-        .screen-lg({
-          width: 32px;
-          height: 32px;
-        });
-
-        .screen-sm({
-          width: 28px;
-          height: 28px;
-        });
-      }
-
-      &.eth {
-        background: @eth;
-      }
-
-      &.polkadot {
-        background-color: @polkadot;
-      }
-
-      &.kusama {
-        background-color: @kusama;
-      }
-
-      &.polygon {
-        background-color: @polygon;
-      }
-
-      &.acala {
-        background-color: @acala;
-      }
-
-      &.moonbeam {
-        background-color: @moonbeam;
-      }
-
-      &.bnb {
-        background-color: @bnb;
-      }
-
-      &.karura {
-        background: @karura;
-      }
-
-      &.moonriver {
-        background-color: @moonriver;
-      }
-
-      &.astar {
-        background-color: @astar;
-      }
-    }
-
-    &-devider {
-      width: 100%;
-      height: 0;
-
-      .screen-md({
-        display: none;
-      });
     }
   }
 
