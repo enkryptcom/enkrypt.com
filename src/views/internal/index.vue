484538cd4a42b47ce4f7210cf57c15239df46846
Mihail Polev <mpolev364@gmail.com>
2022-10-23 19:48:37 +0700

    Fix types

--- src/views/internal/index.vue
+++ src/views/internal/index.vue
@@ -33,58 +33,15 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from "vue";
-import * as objectHash from "object-hash";
+import { computed } from "vue";
+import objectHash from "object-hash";
 import Downloads from "@/components/downloads/index.vue";
 import { useRoute } from "vue-router";
-
-const hash = objectHash;
+import { innerPages } from "@/types/mock";
 
 // TODO: handle and discuss v-html, not safety, parse links?
-const networks = [
-  {
-    path: "polygon",
-    icon: "/src/assets/internal/polygon.network.svg",
-    title: "Polygon wallet",
-    blocks: [
-      {
-        title: "About Enkrypt",
-        paragraps: [
-          "Non-custodial web3 wallets are necessary for building the decentralized internet of the future where user data and privacy are better protected.",
-        ],
-      },
-      {
-        title: "What's Polygon?",
-        paragraps: [
-          "Polygon is a Layer 2 scaling solution that achieves scale by utilizing sidechains for off-chain computation while ensuring asset security using the Plasma framework and a decentralized network of Proof-of-Stake (PoS) validators.",
-          "Polygon strives to solve the scalability and usability issues while not compromising on decentralization and leveraging the existing developer community and ecosystem. Polygon is an off/side chain scaling solution for existing platforms to provide scalability and superior user experience to DApps/user functionalities.",
-          '<a href="https://polygon.technology/" target="_blank" rel="nofollow">https://polygon.technology/</a>',
-        ],
-      },
-    ],
-  },
-  {
-    path: "ethereum",
-    icon: "/src/assets/internal/polygon.network.svg",
-    title: "Ethereum wallet",
-    blocks: [
-      {
-        title: "About Enkrypt",
-        paragraps: [
-          "Non-custodial web3 wallets are necessary for building the decentralized internet of the future where user data and privacy are better protected.",
-        ],
-      },
-      {
-        title: "What's Ethereum?",
-        paragraps: [
-          "Polygon is a Layer 2 scaling solution that achieves scale by utilizing sidechains for off-chain computation while ensuring asset security using the Plasma framework and a decentralized network of Proof-of-Stake (PoS) validators.",
-          "Polygon strives to solve the scalability and usability issues while not compromising on decentralization and leveraging the existing developer community and ecosystem. Polygon is an off/side chain scaling solution for existing platforms to provide scalability and superior user experience to DApps/user functionalities.",
-          '<a href="https://polygon.technology/" target="_blank" rel="nofollow">https://polygon.technology/</a>',
-        ],
-      },
-    ],
-  },
-];
+const networks = innerPages;
+
 var loaded = false;
 var content: {
   path: string;
@@ -103,13 +60,13 @@ if (path) {
 }
 const contentBlocksWithHash = computed(() => {
   if (content && content.blocks) {
-    return content.blocks.map((i: { paragraps: any[] }) => ({
-      ...i,
+    return content.blocks.map((i: { title: string; paragraps: any[] }) => ({
+      title: i.title,
       paragraps: i.paragraps.map((p: any) => ({
         item: p,
-        key: hash(p),
+        key: objectHash(p),
       })),
-      key: hash(i),
+      key: objectHash(i),
     }));
   } else return null;
 });
