141fd861cda20838cf0c83fc4b6e2b05dd5ec932
Oleg Zavrazhin <olegzavrazhin@yahoo.com>
2022-10-25 13:06:54 +0300

    icons fixes

--- src/icons/browser/opera-icon.vue
+++ src/icons/browser/opera-icon.vue
@@ -1,71 +1,87 @@
 <template>
   <svg
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    width="24px"
-    height="24px"
-    viewBox="0 0 24 24"
-    version="1.1"
   >
-    <defs>
-      <linearGradient
-        id="linear0"
-        gradientUnits="userSpaceOnUse"
-        x1="0"
-        y1="0"
-        x2="1"
-        y2="0"
-        gradientTransform="matrix(0,23.258907,-23.258907,0,9.998824,0.392845)"
-      >
-        <stop
-          offset="0"
-          style="stop-color: rgb(100%, 10.588235%, 17.647059%); stop-opacity: 1"
+    <g filter="url(#filter0_d_1035_11488)">
+      <g clip-path="url(#clip0_1035_11488)">
+        <path
+          d="M10.0496 19.7632C8.7224 18.196 7.8632 15.8824 7.8056 13.2832C7.8056 13.276 7.8056 12.724 7.8056 12.7168C7.8632 10.1176 8.7224 7.804 10.0496 6.2368C11.7728 4.0024 14.3312 2.584 17.1872 2.584C18.944 2.584 20.5904 3.1216 21.9968 4.0552C19.8848 2.164 17.1008 1.012 14.0456 1C14.0312 1 14.0144 1 14 1C7.3736 1 2 6.3736 2 13C2 19.4368 7.0664 24.688 13.4288 24.9856C13.6184 24.9952 13.808 25 14 25C17.072 25 19.8752 23.8456 21.9968 21.9472C20.5904 22.8784 18.9464 23.416 17.1872 23.416C14.3312 23.416 11.7728 21.9976 10.0496 19.7632Z"
+          fill="url(#paint0_linear_1035_11488)"
         />
-        <stop
-          offset="0.3"
-          style="stop-color: rgb(100%, 10.588235%, 17.647059%); stop-opacity: 1"
+        <path
+          d="M10.0496 6.23681C11.1512 4.93601 12.572 4.15361 14.1272 4.15361C17.6216 4.15361 20.4512 8.11361 20.4512 13C20.4512 17.8864 17.6192 21.8464 14.1272 21.8464C12.5744 21.8464 11.1512 21.0616 10.0496 19.7632C11.7728 21.9976 14.3312 23.416 17.1872 23.416C18.944 23.416 20.5904 22.8784 21.9968 21.9472C24.4544 19.7488 26 16.5544 26 13C26 9.44561 24.4544 6.25121 21.9968 4.05522C20.5904 3.12161 18.9464 2.58401 17.1872 2.58401C14.3312 2.58401 11.7728 4.00241 10.0496 6.23681Z"
+          fill="url(#paint1_linear_1035_11488)"
         />
-        <stop
-          offset="0.614"
-          style="stop-color: rgb(100%, 10.588235%, 17.647059%); stop-opacity: 1"
-        />
-        <stop
-          offset="1"
-          style="stop-color: rgb(65.490196%, 0%, 7.843137%); stop-opacity: 1"
-        />
-      </linearGradient>
-      <linearGradient
-        id="linear1"
-        gradientUnits="userSpaceOnUse"
-        x1="0"
-        y1="0"
-        x2="1"
-        y2="0"
-        gradientTransform="matrix(0,20.571247,-20.571247,0,16.024364,1.76398)"
-      >
-        <stop
-          offset="0"
-          style="stop-color: rgb(61.176471%, 0%, 0%); stop-opacity: 1"
-        />
-        <stop
-          offset="0.7"
-          style="stop-color: rgb(100%, 29.411765%, 29.411765%); stop-opacity: 1"
-        />
-        <stop
-          offset="1"
-          style="stop-color: rgb(100%, 29.411765%, 29.411765%); stop-opacity: 1"
-        />
-      </linearGradient>
-    </defs>
-    <g id="surface1">
-      <path
-        style="stroke: none; fill-rule: nonzero; fill: url(#linear0)"
-        d="M 12 0 C 5.371094 0 0 5.375 0 12 C 0 18.4375 5.066406 23.6875 11.429688 23.988281 C 11.617188 23.996094 11.808594 24 12 24 C 15.070312 24 17.875 22.84375 19.996094 20.945312 C 18.589844 21.878906 16.945312 22.417969 15.1875 22.417969 C 12.332031 22.417969 9.769531 21 8.050781 18.761719 C 6.722656 17.195312 5.863281 14.882812 5.804688 12.285156 L 5.804688 11.71875 C 5.863281 9.121094 6.722656 6.804688 8.050781 5.238281 C 9.769531 3.003906 12.332031 1.585938 15.1875 1.585938 C 16.945312 1.585938 18.589844 2.121094 19.996094 3.054688 C 17.882812 1.164062 15.101562 0.0117188 12.042969 0 C 12.03125 0 12.015625 0 12 0 Z M 12 0 "
-      />
-      <path
-        style="stroke: none; fill-rule: nonzero; fill: url(#linear1)"
-        d="M 8.050781 5.238281 C 9.152344 3.941406 10.574219 3.15625 12.125 3.15625 C 15.621094 3.15625 18.453125 7.117188 18.453125 12 C 18.453125 16.886719 15.621094 20.847656 12.125 20.847656 C 10.574219 20.847656 9.152344 20.0625 8.050781 18.765625 C 9.769531 21 12.332031 22.417969 15.1875 22.417969 C 16.945312 22.417969 18.589844 21.878906 19.996094 20.949219 C 22.453125 18.75 24 15.558594 24 12 C 24 8.445312 22.453125 5.253906 19.996094 3.054688 C 18.589844 2.125 16.945312 1.585938 15.1875 1.585938 C 12.332031 1.585938 9.769531 3.003906 8.050781 5.238281 "
-      />
+      </g>
     </g>
+    <defs>
+      <filter
+        id="filter0_d_1035_11488"
+        x="0"
+        y="0"
+        width="28"
+        height="28"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_1035_11488"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_1035_11488"
+          result="shape"
+        />
+      </filter>
+      <linearGradient
+        id="paint0_linear_1035_11488"
+        x1="11.9989"
+        y1="1.3913"
+        x2="11.9989"
+        y2="24.6507"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.3" stop-color="#FF1B2D" />
+        <stop offset="0.4381" stop-color="#FA1A2C" />
+        <stop offset="0.5939" stop-color="#ED1528" />
+        <stop offset="0.7581" stop-color="#D60E21" />
+        <stop offset="0.9272" stop-color="#B70519" />
+        <stop offset="1" stop-color="#A70014" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_1035_11488"
+        x1="18.025"
+        y1="2.76223"
+        x2="18.025"
+        y2="23.334"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#9C0000" />
+        <stop offset="0.7" stop-color="#FF4B4B" />
+      </linearGradient>
+      <clipPath id="clip0_1035_11488">
+        <rect width="24" height="24" fill="white" transform="translate(2 1)" />
+      </clipPath>
+    </defs>
   </svg>
 </template>
