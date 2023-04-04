792d33a7b65788b6d9773b2780661b11a706f66d
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-04-04 13:03:28 -0700

    devop: switch

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import requireTransform from "vite-plugin-require-transform";
 import { StaticNetworkPages } from "./static-page-plugin";
-import { StrapiData } from "./download-strapi-data";
 import viteImagemin from "vite-plugin-imagemin";
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,6 @@ export default defineConfig({
     global: {},
   },
   plugins: [
-    StrapiData(),
     vue(),
     requireTransform({}),
     StaticNetworkPages(),
